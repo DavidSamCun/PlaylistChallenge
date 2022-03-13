5ed3ceac57fce9c9e1fb33431cf7e369d5970203
David <davidsamcun@gmail.com>
2022-03-13 19:19:02 -0400

    backRepaste

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,45 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+        int forward = forwardScan(startIndex, selection);
+        int backward = backwardScan(startIndex, selection);
+        if(forward < backward) {
+            return forward;
+        }
+        return backward;
+    }
+
+    public Integer forwardScan(Integer startIndex, String selection){
+
+        int spot = startIndex;  //Go forward
+        int forward = 0;    //forward count
+        int backward = 0;   //backward count
+        int selectSpot = 0;
+
+        for(int i = startIndex; !playList[i].equals(selection); i ++){
+            if (i == playList.length -1){
+                i = -1;
+            }
+            forward++;
+        }
+        System.out.println(forward);
+
+        return forward;
+    }
+
+    public Integer backwardScan(Integer startIndex, String selection){
+
+        int backward = 0;
+        for(int i = startIndex; !playList[i].equals(selection); i --){
+            if (i == 0){
+                i = playList.length;
+            }
+            backward++;
+        }
+        System.out.println(backward);
+
+
+        return backward;
     }
 }
